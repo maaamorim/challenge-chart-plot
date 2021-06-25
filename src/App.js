f52fa49c48d67b1b2cb089eec8cb46bc0bc5967a
Mateus Antonio <mateuscfc8@outlook.com>
2021-06-25 18:29:34 +0000

    chore: Add ESLint, MaterialUI and some components
     * Added ESLint, with the standard configuration;
     * Added MaterialUI;
     * Added Source Sans Pro font globally and on MaterialUI;
     * Added Header and Footer base components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-function App() {
+import React from 'react'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Source Sans Pro',
+      'sans-serif'
+    ].join(',')
+  }
+})
+
+function App () {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Footer />
     </div>
-  );
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button } from '@material-ui/core'
+
+class Footer extends Component {
+  render () {
+    return (
+      <footer>
+        <Button variant="contained" color="primary">GENERATE CHART</Button>
+      </footer>
+    )
+  }
+}
+
+export default Footer
